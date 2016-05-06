b62443a359803f131a1271b3feb8693dc39ddf85
shaozhuchuang <shaozhuchuang@126.com>
2016-05-06 16:27:03 +0800

    plot phase before sanitization

---
+++ mycode/plot_phase_beforeSanitization.m
@@ -0,0 +1,25 @@
+function plot_phase_beforeSanitization(csi_trace,packet1,packet2)
+	path('../linux-80211n-csitool-supplementary/matlab', path);
+	csi_entry1 = csi_trace{packet1};
+	csi_entry2 = csi_trace{packet2};
+	csi1 = get_scaled_csi(csi_entry1);
+	csi1 = csi1(1, :, :);
+	csi1 = squeeze(csi1);
+	
+	csi2 = get_scaled_csi(csi_entry2);
+	csi2 = csi2(1, :, :);
+	csi2 = squeeze(csi2);
+	
+	ang1=angle(csi1);
+    ang2=angle(csi2);
+    unwrapAng1=unwrap(ang1,pi,2);
+    unwrapAng2=unwrap(ang2,pi,2);
+    hold on;
+    plot(unwrapAng1(1,:),'r');
+    plot(unwrapAng1(2,:),'g');
+    plot(unwrapAng1(3,:),'b');
+    plot(unwrapAng2(1,:),'--r');
+    plot(unwrapAng2(2,:),'--g');
+    plot(unwrapAng2(3,:),'--b');
+    hold off;
+end

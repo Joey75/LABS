a0235a9bf9a9a67f7634f4d6e227646623644de1
Zhuchuang Shao <shaozhuchuang@126.com>
2016-05-25 15:09:15 +0800

    modify the readfile to filter the empty csi_trace

--- mycode/readfile.m
+++ mycode/readfile.m
@@ -1,4 +1,12 @@
 function csi_trace = readfile(filepath)
 	path('../linux-80211n-csitool-supplementary/matlab', path);
-	csi_trace = read_bf_file(filepath);
+	temp = read_bf_file(filepath);
+    flag=cellfun(@isempty,temp)
+    k=1;
+    for i=1:size(temp,1)
+        if flag(i,1)==0
+            csi_trace(k,1)=temp(i,1);
+            k=k+1;
+        end
+    end
 end

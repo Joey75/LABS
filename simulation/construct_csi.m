f9baf6c8f697267d5dae7c6f7f2fcbfee1051298
Zhuchuang Shao <shaozhuchuang@126.com>
2016-06-22 09:58:32 +0800

    add snr

--- simulation/construct_csi.m
+++ simulation/construct_csi.m
@@ -1,5 +1,6 @@
 function csi=construct_csi(aoa,tof)
     sub_freq_delta=1*10^6;
+    snr=100;
     for m=1:1:3
         for l=1:1:length(aoa)
             a(m,l)=exp(-1i*2*pi*(m-1)*sin(aoa(l)*pi/180)/2);
@@ -10,6 +11,7 @@ function csi=construct_csi(aoa,tof)
             x(l,i)=exp(-1i*2*pi*(i-1)*sub_freq_delta*tof(l)*10^(-9));
         end
     end
-    noise=(randn(3,30)+j*randn(3,30))/sqrt(2); 
-    csi=a*x+noise;
+    noise=(randn(3,30)+1i*randn(3,30))/sqrt(2);
+    amp=sqrt(cov(noise(1,:))*10^(snr/10));
+    csi=amp*a*x+noise;
 end

f9c016acfabf59337f9b2f0fd46ca45298a37850
Zhuchuang Shao <shaozhuchuang@126.com>
2016-06-22 11:31:19 +0800

    update music algorithm: modify search peak

--- simulation/aoa_tof_music.m
+++ simulation/aoa_tof_music.m
@@ -18,7 +18,7 @@ function [estimated_aoas, estimated_tofs] = aoa_tof_music(x, sub_freq_delta)
     % Peak search
     % Angle in degrees (converts to radians in phase calculations)
     %% TODO: Tuning theta too??
-    theta = -90:1:90; 
+    theta = -90:0.5:90; 
     % time in milliseconds
     %% TODO: Tuning tau....
     %tau = 0:(1.0 * 10^-9):(50 * 10^-9);
@@ -46,31 +46,9 @@ function [estimated_aoas, estimated_tofs] = aoa_tof_music(x, sub_freq_delta)
 	title('AoA and ToF Estimation from Modified MUSIC Algorithm')
 	grid on
 
-    % Find AoA peaks
-    [~, aoa_peak_indices] = findpeaks(Pmusic(:, 1));
-    estimated_aoas = theta(aoa_peak_indices);
+    % Find AoA && TOF peaks
+    [estimated_aoas, estimated_tofs]=find_music_peak(Pmusic,theta,tau);
     
-    % Find ToF peaks
-    time_peak_indices = zeros(length(aoa_peak_indices), length(tau));
-    % AoA loop (only looping over peaks in AoA found above)
-    for ii = 1:length(aoa_peak_indices)
-        aoa_index = aoa_peak_indices(ii);
-        % For each AoA, find ToF peaks
-        [peak_values, tof_peak_indices] = findpeaks(Pmusic(aoa_index, :));
-        if isempty(tof_peak_indices)
-
-            tof_peak_indices = 1;
-        end
-        % Pad result with -1 so we don't have a jagged matrix (and so we can do < 0 testing)
-        negative_ones_for_padding = -1 * ones(1, length(tau) - length(tof_peak_indices));
-        time_peak_indices(ii, :) = horzcat(tau(tof_peak_indices), negative_ones_for_padding);
-    end
-
-    % Set return values
-    % AoA is now a column vector
-    estimated_aoas = transpose(estimated_aoas);
-    % ToF is now a length(estimated_aoas) x length(tau) matrix, with -1 padding for unused cells
-    estimated_tofs = time_peak_indices;
 end
 
 %% Computes the steering vector for SpotFi. 
